2cbdc7546f7609a816387b946aa7e5f20f78b972
Mingze Zheng <mzheng15@jh.edu>
2021-11-13 20:26:02 -0500

    Update the unit_test
    Correct the unit_test due to the change of the main script.

--- unit_test.py
+++ unit_test.py
@@ -3,12 +3,14 @@ import lazor_project_final
 
 
 class TestStringMethods(unittest.TestCase):
+    '''
+    This test module use 'mad_1.bff'
+    '''
 
     def test_read(self):
         '''
         This function can test the read funciton.
         '''
-        # mad_1.bff
         fullgrid = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
                     ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
                     ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
@@ -43,10 +45,6 @@ class TestStringMethods(unittest.TestCase):
                     ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
                     ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
                     ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
-        grid = [['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o']]
         A_blocks = 2
         B_blocks = 0
         C_blocks = 1
@@ -61,94 +59,36 @@ class TestStringMethods(unittest.TestCase):
                        ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
                        ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
                        ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
-        self.assertEqual(lazor_project_final.solver(grid=fullgrid, init_laz_list=lazorlist,
-                         holelist=holelist, a=A_blocks, b=B_blocks, c=C_blocks, init_grid=grid)[0], solved_grid)
+        self.assertEqual(lazor_project_final.find_path(grid=fullgrid, A_num=A_blocks, B_num=B_blocks,
+                        C_num=C_blocks, lazorlist=lazorlist, holelist=holelist)[2], solved_grid)
 
     def test_path(self):
         '''
         This function can test the lazor path
         '''
-        fullgrid = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
-        grid = [['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o']]
-        A_blocks = 2
-        B_blocks = 0
-        C_blocks = 1
-        lazorlist = [[2, 7, 1, -1]]
-        holelist = [[3, 0], [4, 3], [2, 5], [4, 7]]
         lazor_path = [[[2, 7, 1, -1], [3, 6, 1, -1], [4, 5, 1, -1],
                        [5, 4, 1, -1], [6, 3, 1, -1], [5, 2, -1, -1],
-                       [4, 3, -1, 1], [3, 4, -1, 1], [2, 5, -1, 1],
-                       [3, 6, 1, 1], [4, 7, 1, 1], [5, 8, 1, 1]],
-                      [[4, 1, -1, -1], [3, 0, -1, -1]]]
-        self.assertEqual(lazor_project_final.solver(grid=fullgrid, init_laz_list=lazorlist,
-                         holelist=holelist, a=A_blocks, b=B_blocks, c=C_blocks, init_grid=grid)[1], lazor_path)
+                       [5, 2, -1, 1], [4, 3, -1, 1], [3, 4, -1, 1],
+                       [2, 5, -1, 1], [3, 6, 1, 1], [4, 7, 1, 1],
+                       [5, 8, 1, 1]], [[4, 1, -1, -1], [3, 0, -1, -1]]]
+        self.assertEqual(lazor_project_final.solver(
+            'mad_1.bff')[1], lazor_path)
 
     def test_permu(self):
         '''
         This function can test the filled in permutation of blocks
         '''
-        fullgrid = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
-        grid = [['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o']]
-        A_blocks = 2
-        B_blocks = 0
-        C_blocks = 1
-        lazorlist = [[2, 7, 1, -1]]
-        holelist = [[3, 0], [4, 3], [2, 5], [4, 7]]
         permu = ['o', 'o', 'C', 'o', 'o', 'o', 'o',
                  'A', 'A', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
-        self.assertEqual(lazor_project_final.solver(grid=fullgrid, init_laz_list=lazorlist,
-                         holelist=holelist, a=A_blocks, b=B_blocks, c=C_blocks, init_grid=grid)[2], permu)
+        self.assertEqual(lazor_project_final.solver('mad_1.bff')[2], permu)
 
     def test_answ(self):
         '''
         This funciton can test the result
         '''
-        fullgrid = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-                    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'],
-                    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
-        grid = [['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o']]
-        A_blocks = 2
-        B_blocks = 0
-        C_blocks = 1
-        lazorlist = [[2, 7, 1, -1]]
-        holelist = [[3, 0], [4, 3], [2, 5], [4, 7]]
-        answ = [['o', 'o', 'C', 'o'],
-                ['o', 'o', 'o', 'A'],
-                ['A', 'o', 'o', 'o'],
-                ['o', 'o', 'o', 'o']]
-        self.assertEqual(lazor_project_final.solver(grid=fullgrid, init_laz_list=lazorlist,
-                         holelist=holelist, a=A_blocks, b=B_blocks, c=C_blocks, init_grid=grid)[3], answ)
+        answ = [['o', 'o', 'C', 'o'], ['o', 'o', 'o', 'A'],
+                ['A', 'o', 'o', 'o'], ['o', 'o', 'o', 'o']]
+        self.assertEqual(lazor_project_final.solver('mad_1.bff')[0], answ)
 
 
 if __name__ == '__main__':
